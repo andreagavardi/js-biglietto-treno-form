3398252f76b43ec458c0b356b4ea455dea85fa8c
andreagavardi <andreagavardi89@gmail.com>
2021-05-03 17:16:42 +0200

    impostato layout html e css

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+
+
+
+/* Partendo dall'esercizio fatto tempo fa, aggiungiamo la parte visiva come da screenshot
+Creiamo un finto biglietto del treno con:
+Nome passeggero
+Codice treno (numero casuale tra 90000 e 100000 escluso)
+Numero carrozza
+Prezzo calcolato
+Categoria selezionata dall'utente
+Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form. */
+
+var nomeUtenteEl = document.getElementById("nome_utente");
+var kmUtenteEl = documen.getElementById("km_percorso");
